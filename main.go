7dc384e327c2ca6bd2efc485692515f815cee25b
Sergei Shabalov <shabalov@white-rock.group>
2025-08-04 20:59:32 +0400

    parallel reading

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/binary"
 	"flag"
 	"fmt"
@@ -12,10 +13,12 @@ import (
 )
 
 var inputFilePathFlag = flag.String("i", "input.txt", "input file path with IP addresses")
+var inputNumReadWorkers = flag.Int("w", 6, "number of read worker")
+var inputIPChunkSize = flag.Int("c", 10000, "size of raw ip chunk that is passed to workers")
 
 func main() {
 	flag.Parse()
-	if inputFilePathFlag == nil {
+	if inputFilePathFlag == nil || inputNumReadWorkers == nil || inputIPChunkSize == nil {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -44,21 +47,51 @@ func countIPAddressesBitMap(filePath string) (int, error) {
 	// let addr be 0x12345678, it's already a kind of index for our bitmap
 	// elem of bitmap - 64 bits, can hold 64 (2^6) unique addresses or last 6 bits
 	addressesMap := make([]uint64, 2<<(32-6))
+
+	numReadWorkers := uint(*inputNumReadWorkers)
+	readChan := make(chan [][]byte, 100)
+	for range numReadWorkers {
+		go func() {
+			for ipRawChunk := range readChan {
+				for _, ipRaw := range ipRawChunk {
+					if len(ipRaw) == 0 {
+						// reached end of chunk
+						break
+					}
+					ipAddr := parseIPAddr(ipRaw)
+					mapIndex := ipAddr >> 6
+					// take last 6 bits of addr
+					mapElemShift := ipAddr & 0x3f
+					// and shift 1 to the left by this value to find bit index
+					addressesMap[mapIndex] |= 1 << mapElemShift
+				}
+			}
+		}()
+	}
+
+	ipChunkSize := *inputIPChunkSize
+	ipRawChunk := make([][]byte, ipChunkSize)
+	i := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		ipAddr, errParse := parseIPAddr(scanner.Text())
-		if errParse != nil {
-			return 0, err
+		ipRaw := scanner.Bytes()
+		ipRawChunk[i] = make([]byte, len(ipRaw))
+		copy(ipRawChunk[i], ipRaw)
+		i++
+		if i == ipChunkSize {
+			readChan <- ipRawChunk
+			ipRawChunk = make([][]byte, ipChunkSize)
+			i = 0
 		}
-		mapIndex := ipAddr >> 6
-		// take last 6 bits of addr
-		mapElemShift := ipAddr & 0x3f
-		// and shift 1 to the left by this value to find bit index
-		addressesMap[mapIndex] |= 1 << mapElemShift
+	}
+	if i > 0 {
+		readChan <- ipRawChunk
 	}
 	if err = scanner.Err(); err != nil {
 		return 0, fmt.Errorf("scan file %s: %w", filePath, err)
 	}
+
+	close(readChan)
 
 	result := 0
 	for _, addressesElem := range addressesMap {
@@ -68,12 +101,22 @@ func countIPAddressesBitMap(filePath string) (int, error) {
 	return result, nil
 }
 
-func parseIPAddr(line string) (uint32, error) {
+func parseIPAddr(line []byte) uint32 {
+	ipAddrPartsBytes := bytes.Split(line, []byte{'.'})
 	var ipAddrParts [4]byte
-	_, err := fmt.Sscanf(line, "%d.%d.%d.%d", &ipAddrParts[0], &ipAddrParts[1], &ipAddrParts[2], &ipAddrParts[3])
-	if err != nil {
-		return 0, fmt.Errorf("parse ip addr %s: %w", line, err)
+	for i := range 4 {
+		ipAddrPart := unsignedByteAtoi(ipAddrPartsBytes[i])
+		ipAddrParts[i] = byte(ipAddrPart)
 	}
 	// we can use any endianness
-	return binary.BigEndian.Uint32(ipAddrParts[:]), nil
+	return binary.BigEndian.Uint32(ipAddrParts[:])
+}
+
+// simplified implementation of strings.Atoi for byte slice
+func unsignedByteAtoi(raw []byte) int {
+	result := 0
+	for i := 0; i < len(raw); i++ {
+		result = result*10 + int(raw[i]-'0')
+	}
+	return result
 }
